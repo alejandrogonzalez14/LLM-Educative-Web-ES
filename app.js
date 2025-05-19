e1fceccad6edfa51adf3da58b2a628a04b77c1e1
Alejandro González <115491795+alejandrogonzalez14@users.noreply.github.com>
2025-05-19 12:38:27 +0200

    fix eslint 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let temaActual = 0;
 const temes = [
   { backgroundColor: '#f0f8ff', color: '#000080' }, // Tema clar (blau clar amb text blau marí)
   { backgroundColor: '#ffe4e1', color: '#8b0000' }, // Tema pastel (rosa pàl·lid amb text vermell fosc)
-  { backgroundColor: '#ffefd5', color: '#8b4513' } // Tema pastel (beix pàl·lid amb text marró)
+  { backgroundColor: '#ffefd5', color: '#8b4513' }, // Tema pastel (beix pàl·lid amb text marró)
 ];
 
 document.getElementById('themeButton').addEventListener('click', () => {
